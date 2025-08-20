2caec7a8fc0a999476fbc7b825f8100ae26a7450
Mike Herman <mhermedu@proton.me>
2025-08-20 11:28:59 -0400

    netlify flutter build
    w/ latest flutter version

--- netlify_build.sh
+++ netlify_build.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
-FLUTTER_VERSION="${FLUTTER_VERSION:-3.22.2}"
-
-curl -L "https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_${FLUTTER_VERSION}-stable.tar.xz" \
-  | tar -xJ
+curl -L "https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_${FLUTTER_VERSION}-stable.tar.xz" | tar -xJ
 export PATH="$PWD/flutter/bin:$PATH"
 
 flutter --version
